d9caab751b91783645714346ff75040fe103dba5
uithen <uithen111217@gmail.com>
2021-04-02 17:58:33 +0800

    初始化项目: 划分目录结构、重置&添加全局样式、配置l路径别名

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo } from 'react'
 
-function App() {
+export default memo(function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>App Header</h3>
+      <h3>App Content</h3>
+      <h3>App Footer</h3>
     </div>
-  );
-}
-
-export default App;
+  )
+})
